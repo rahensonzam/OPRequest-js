b8d2ce9e6be3d0962e2bf7ffc524543f436f6ded
rahensonzam <rahensonzam@gmail.com>
2021-09-28 11:04:50 +0200

    Amend f532437b Adjust dummy data - user IDs

--- scripts/apiKeyListFile.js
+++ scripts/apiKeyListFile.js
@@ -1,8 +1,8 @@
 function passwordToApiKey(user, password){
-    const apiKeyList = [{"user": "5", "name": "admin", "password": "", "apikey": ""},
-    {"user": "4", "name": "Alice", "password": "1234", "apikey": "1234567890123456789012345678901234567890123456789012345678901234"},
-    {"user": "5", "name": "Bob", "password": "1234", "apikey": "1234567890123456789012345678901234567890123456789012345678901234"},
-    {"user": "6", "name": "Carrol", "password": "1234", "apikey": "1234567890123456789012345678901234567890123456789012345678901234"}]
+    const apiKeyList = [{"user": "4", "name": "admin", "password": "", "apikey": ""},
+    {"user": "6", "name": "Alice", "password": "1234", "apikey": "1234567890123456789012345678901234567890123456789012345678901234"},
+    {"user": "7", "name": "Bob", "password": "1234", "apikey": "1234567890123456789012345678901234567890123456789012345678901234"},
+    {"user": "8", "name": "Carrol", "password": "1234", "apikey": "1234567890123456789012345678901234567890123456789012345678901234"}]
 
     for (let i = 0; i <= apiKeyList.length - 1; i++) {
         if ((user === apiKeyList[i].user) && (password === apiKeyList[i].password)) {
